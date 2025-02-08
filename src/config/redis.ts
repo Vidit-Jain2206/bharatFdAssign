482cafe1a003b401ee5b09048f1af60f899596e2
Vidit-Jain2206 <jainvidit203@gmail.com>
2025-02-08 19:21:30 +0530

    feat: add Docker and docker-compose configuration for containerization

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,10 +1,5 @@
 import { Redis } from "ioredis";
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: parseInt(process.env.REDIS_PORT || "6379"),
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
-});
+const redis = new Redis(process.env.REDIS_URL || "");
 
 export default redis;
